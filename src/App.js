ecfa049999d9f150972e81944175a6ba4c4b85e0
FLAVIO AMORUSO <flavioamoruso6.com>
2024-01-27 21:30:59 +0100

    inizio progetto e creato il context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, Sidebar, Footer } from "./components";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import AboutScreen from "./screen/AboutScreen";
+import ContactScreen from "./screen/ContactScreen";
+import SingleCocktailScreen from "./screen/SingleCocktailScreen";
+import ErrorScreen from "./screen/ErrorScreen";
+import { useGlobalContext } from "./context";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const prova = useGlobalContext
+  console.log(prova);
+  return <div className='App'>
+    App
+  </div>;
 }
 
 export default App;
