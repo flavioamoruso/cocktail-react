abeae0d80bb612d679a7eabbfa82bfb86238d133
FLAVIO AMORUSO <flavioamoruso6.com>
2024-01-28 17:33:42 +0100

    aboutUs page in corso

--- src/screen/AboutScreen.js
+++ src/screen/AboutScreen.js
@@ -6,8 +6,56 @@ import teamImg from "../assets/image/team-img.jpg";
 
 const AboutScreen = () => {
   return <>
-  <Hero img={image} />
-  </>
-};
-
-export default AboutScreen;
+  <Hero img={image}>
+  <section className="about-hero">
+  <div className="about-text">
+  <div className="topline"></div>
+  <h3>
+  <q>
+  Grande è la fortuna di chi possiede un buon cocktail e non si ubriaca con 2
+  </q>
+  </h3>
+  <div className="underline"></div>
+  </div>
+  </section>
+  </Hero>
+  <section className="about-value">
+  <div className="container about-value-content">
+  <h3 className="brand-secondary-color">Gli obiettivi principali</h3>
+  <div className="card-section">
+  {
+    valueCards.map((card) => {
+      return (
+        <Card key={card.title} {...card} className= {"valueCards"}/>
+        );
+      })
+    }
+    </div>
+    </div>
+    </section>
+    <section className="about-team">
+    <div className="team-img" 
+    style={{
+      background: `url(${teamImg})`,
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }}
+    ></div>
+    <div className="container about-team-content">
+    <h3 className="brand-secondary-color">
+    Il Nostro Staff
+    </h3>
+    {
+      teamCards.map((card) => {
+        return (
+          <Card key={card.title} {...card} className= {"valueCards"}/>
+          );
+        })
+      }
+      </div>
+      </section>
+      </>
+    };
+    
+    export default AboutScreen;
